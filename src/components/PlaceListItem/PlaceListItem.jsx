03f4679e0715d10c55135b9b3605f1bd8a9d5c4b
Skunk-XCP <skunk-xcp@hotmail.fr>
2023-10-23 14:10:50 +0200

    fix alt img PlaceListItem

--- src/components/PlaceListItem/PlaceListItem.jsx
+++ src/components/PlaceListItem/PlaceListItem.jsx
@@ -8,7 +8,7 @@ export function PlaceListItem({ id, title, cover }) {
             <article className={s.location_card}>
                 <span className={s.gradient_overlay}></span>
                 <figure className={s.card_figure}>
-                    <img className={s.cover} src={cover} alt="" />
+                    <img className={s.cover} src={cover} alt={title} />
                     <figcaption>
                         <h2 className={s.location_title}>{title}</h2>
                     </figcaption>
