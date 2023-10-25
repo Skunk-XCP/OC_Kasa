e668b67b9236fd40b022091f3a5fe908e85f0905
Skunk-XCP <skunk-xcp@hotmail.fr>
2023-10-25 13:37:15 +0200

    fix responsive

--- src/components/InfoButton/InfoButton.jsx
+++ src/components/InfoButton/InfoButton.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 // InfoButton.js
 export function InfoButton({ title, content, isLarge }) {
     // On initialise un état local showInfo à null. Il servira à savoir quel bouton est ouvert
-    const [showInfo, setShowInfo] = useState(null);
+    const [showInfo, setShowInfo] = useState(false);
 
     return (
         // div qui change de style en fonction de la prop isLarge
@@ -13,21 +13,21 @@ export function InfoButton({ title, content, isLarge }) {
             {/* Bouton dont la valeur va changer en fonction du bouton cliqué */}
             <button
                 className={s.toggle_button}
-                onClick={() => setShowInfo(showInfo === title ? null : title)}
+                onClick={() => setShowInfo(!showInfo)}
             >
                 {title}
-                <i className={`bi bi-chevron-down ${showInfo === title ? s.rotate : s.initial} ${s.chevronIcon}`}></i>
+                <i className={`bi bi-chevron-down ${showInfo ? s.rotate : s.initial} ${s.chevronIcon}`}></i>
             </button>
 
             {/* Si showInfo est égal au titre du bouton, on affiche le contenu */}
-            {showInfo === title && (
+            {showInfo && (
                 <div className={s.content}>
 
                     {/* Si le contenu est un tableau, on le map en une list HTML */}
                     {/* Sinon, on affiche simplement le contenu */}
                     {Array.isArray(content)
                         ? <ul>{content.map((el, i) => <li key={i}>{el}</li>)}</ul>
-                        : content
+                        : <span>{content}</span>
                     }
                 </div>
             )}
